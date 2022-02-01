594899341fc38a59bcdf66fe85dc1395b3dc8ea7
StephaneRavet <ravet.st@gmail.com>
2022-02-01 14:33:36 +0100

    Chapitre 10 : Les props

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 function Counter(props) {
   const [counter, setCounter] = useState(0)
+
+  const [historique, setHistorique] = useState([])
+  const histoCounter = useCallback((value) => {
+    setHistorique([...historique, counter])
+    setCounter(value)
+  }, [historique, counter])
+
   return (
     <div>
       {counter} : Le compteur est {counter % 2 ? 'impair' : 'pair'}.<br />
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => histoCounter(counter - 1)}>-</button>
+      <button onClick={() => histoCounter(counter + 1)}>+</button>
+      <br />
+      {historique.map((histo, i) => <div key={i}>valeur {i} : {histo}</div>)}
     </div>
   );
 }
