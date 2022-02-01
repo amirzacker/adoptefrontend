c949190716d0d7049ce6c44e17f36beef358af9f
StephaneRavet <35328743+StephaneRavet@users.noreply.github.com>
2022-02-01 10:04:00 +0100

    Chapitre 9 : L’affichage de listes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ function Counter(props) {
   const [counter, setCounter] = useState(0)
   return (
     <div>
-      {counter}<br/>
+      {counter} : Le compteur est {counter % 2 ? 'impair' : 'pair'}.<br />
       <button onClick={() => setCounter(counter - 1)}>-</button>
       <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
