7abe81a73723e6a93d4bca9bd57bc372ea4ad8c4
StephaneRavet <35328743+StephaneRavet@users.noreply.github.com>
2022-02-01 10:17:38 +0100

    Chapitre 12 : Factorisation de code

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import { useCallback, useState, useEffect } from 'react';
+
+const users = ['Eva', 'Aude', 'Anne', 'Marc', 'Sansom']
 
 function UserList() {
-  const users = [
-    'Eva',
-    'Aude',
-    'Anne',
-    'Marc'
-  ]
+  const [searchCriteria, setSearchCriteria] = useState('')
+  const handleSearch = useCallback(event => setSearchCriteria(event.target.value), [])
+
+  const [filteredUsers, setFilteredUsers] = useState([])
+  useEffect(() => {
+    setFilteredUsers(users.filter(user => user.toLowerCase().includes(searchCriteria.toLowerCase())))
+  }, [searchCriteria])
+
   return (
     <div>
       <h1>Liste des utilisateurs</h1>
+      <input type="text" className='form-control' placeholder='Recherche' onChange={handleSearch} />
       <ul>
-        {users.map((user, i) => <li key={i}>{user}</li>)}
+        {filteredUsers.map((user, i) => <li key={i}>{user}</li>)}
       </ul>
     </div>
   );
