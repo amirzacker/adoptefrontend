5a0f5718f93aed6b7f9ce0e68231a79226040fca
amir_zacker <abgurene4@gmail.com>
2022-12-16 23:11:21 +0100

    login error

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
@@ -9,7 +9,9 @@ import { Link } from "react-router-dom";
 export default function Login() {
   const email = useRef();
   const password = useRef();
-  const { isFetching, dispatch } = useContext(AuthContext);
+  const { isFetching, dispatch, error } = useContext(AuthContext);
+  const [authError, setAuthError] = useState('');
+ 
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -18,6 +20,11 @@ export default function Login() {
       dispatch
     );
   };
+  
+  useEffect(() => {
+  error ? setAuthError("password and user don't match") : setAuthError("") 
+  }, [error]);
+
 
   return (
     <main>
@@ -26,9 +33,16 @@ export default function Login() {
             <form className="login" onSubmit={handleClick}>
             <img src="assets/logos/logodef.svg" alt="adopte-logo" className="adopte-logo-login"/>
                 <p className="welcome">Bienvenue</p>
-                <input type="email" name="email" placeholder="Email" ref={email} required/><br/>
+                <input type="email" name="email" placeholder="Email" ref={email} onChange={(e) => (e.target.value)} required/><br/>
                 <input type="password" name="password" placeholder="Mot de passe" required minLength="6" ref={password}/><br/>
-                <input type="submit" name="submit" value="Connexion" disabled={isFetching} /><br/>
+                {authError && <div className="alert alert-danger">{authError}</div>}
+                <input type="submit" name="submit" value="Connexion" disabled={isFetching} /> 
+                {isFetching ? (
+                <CircularProgress color="secondary" size="30px" />
+              ) : (
+                ""
+              )}
+                <br/>
               <Link to="/registerStudent">Vous etes un nouveau étudiant?</Link><br/>
               <Link to="/registerCompany">une entreprise?</Link>
             </form>
